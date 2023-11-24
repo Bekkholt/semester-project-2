fe1d2c91f84e1acaf0ee8ef09f5ddb1a88c0ae6b
Tina <tina.bekkholt@hotmail.com>
2023-11-24 12:01:30 +0100

    logout done

---
+++ src/js/logout.js
@@ -0,0 +1,9 @@
+const logoutButton = document.querySelector("#logout");
+
+logoutButton.addEventListener("click", onClick);
+
+//** Clears localstorage and returns to login page */
+function onClick() {
+  localStorage.clear();
+  location.href = "/";
+}
